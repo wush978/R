4efe1bf18d62de5fb249260507b8b9fba53e3ec2
Wush Wu <wush978@gmail.com>
2017-02-22 14:56:23 +0800

    use a variable "repo" to make it easier to create mirror

--- init-swirl.R
+++ init-swirl.R
@@ -1,14 +1,15 @@
+repos <- "http://wush978.github.io/R"
 local({
   check_package <- function(pkg) {
     if (!suppressWarnings(suppressMessages(require(pkg, character.only = TRUE)))) {
-      install.packages(pkg, repos = "http://wush978.github.io/R")
+      install.packages(pkg, repos = repos)
     }
   }
   lapply(c("rappdirs", "bitops", "curl", "httpuv"), check_package)
-  utils::install.packages("swirl", repos = "http://wush978.github.io/R")
+  utils::install.packages("swirl", repos = repos)
 })
 library(swirl)
 library(curl)
 library(methods)
 try(uninstall_course("DataScienceAndR"), silent=TRUE)
-install_course_github("wush978", "DataScienceAndR", "course")
+install_course_url(sprintf("%s/course/DataScienceAndR.zip", repos))
