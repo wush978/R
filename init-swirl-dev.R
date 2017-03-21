f76322fb5d44c6c2c8ca0866e0cdc8ebe78635cd
Wush Wu <wush978@gmail.com>
2017-03-22 02:27:22 +0800

    Create init-swirl-dev.R

---
+++ init-swirl-dev.R
@@ -0,0 +1,10 @@
+if (!require(remotes)) {
+  install.packages("remotes", repos = "http://cran.csie.ntu.edu.tw")
+}
+remotes::install_github("wush978/pvm@dcbcb71f529e805493058732b70505759a8f0501")
+pvm::import.packages("https://raw.githubusercontent.com/wush978/pvm-list/master/dsr.yml")
+library(swirl)
+library(curl)
+library(methods)
+try(uninstall_course("DataScienceAndR"), silent=TRUE)
+install_course_url(sprintf("%s/course/DataScienceAndR.zip", repos))
