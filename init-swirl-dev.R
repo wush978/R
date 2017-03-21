4331208b55498f0c74f9d8a8d2024706e836bf2b
Wush Wu <wush978@gmail.com>
2017-03-22 02:40:00 +0800

    Update init-swirl-dev.R

--- init-swirl-dev.R
+++ init-swirl-dev.R
@@ -1,7 +1,5 @@
-if (!require(remotes)) {
-  install.packages("remotes", repos = "http://cran.csie.ntu.edu.tw")
-}
-remotes::install_github("wush978/pvm@dcbcb71f529e805493058732b70505759a8f0501")
+if (!require(remotes)) install.packages("remotes", repos = "http://cran.csie.ntu.edu.tw")
+if (!require(pvm)) remotes::install_github("wush978/pvm@dcbcb71f529e805493058732b70505759a8f0501")
 pvm::import.packages("https://raw.githubusercontent.com/wush978/pvm-list/master/dsr.yml")
 utils::install.packages("swirl", repos = "https://wush978.github.io/R")
 library(swirl)
