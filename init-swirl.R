995b791fd6284e9fb980921884d8a903c1a35e16
Wush Wu <wush978@gmail.com>
2017-04-23 14:36:16 +0800

    Update init-swirl.R

--- init-swirl.R
+++ init-swirl.R
@@ -1,3 +1,16 @@
+if (package_version(R.version) > package_version("3.3.3")) local({
+  msg <- sprintf("你的R的版本(%s)剛上線，翻轉教室需要針對這個版本做調校。請先到 https://mran.revolutionanalytics.com/snapshot/2017-04-01/ 安裝3.3.3版本的R", package_version(R.version))
+  Encoding(msg) <- "UTF-8"
+  stop(msg)
+})
+
+if (package_version(R.version) < package_version("3.2.3")) local({
+  msg <- sprintf("The version of R (%s) is not compatible with the course content. Please visit https://mran.revolutionanalytics.com/snapshot/2017-04-01/ to upgrade your R.", package_version(R.version))
+  Encoding(msg) <- "UTF-8"
+  stop(msg)
+})
+
+
 local({
   repos <- "https://wush978.github.io/R"
   if (!require(remotes)) install.packages("remotes", repos = "http://cran.csie.ntu.edu.tw")
