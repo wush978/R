36f5c627d2b5a14879b5a47c8763883ed5457b80
Wush Wu <wush978@gmail.com>
2023-01-29 11:21:14 +0800

    remove pvm

--- init-HahowRTutorial.R
+++ init-HahowRTutorial.R
@@ -7,15 +7,8 @@ if (package_version(R.version) < package_version("3.2.3")) local({
 
 local({
   repos <- "https://wush978.github.io/R"
-  if (!suppressWarnings(require(remotes))) install.packages("remotes", repos = "http://cran.csie.ntu.edu.tw")
-  if (!suppressWarnings(require(pvm))) utils::install.packages("pvm", repos = "https://wush978.github.io/R", type = "source")
-  pvm::import.packages(sprintf("https://raw.githubusercontent.com/wush978/pvm-list/master/dsr-%s.yml", package_version(R.version)))
-  pkgs.repos <- available.packages(contrib.url(repos, "source"))
-  pkgs.installed <- installed.packages()
-  check.result <- try(pkgs.repos["swirl", "Version"] == pkgs.installed["swirl", "Version"], silent = TRUE)
-  if (!isTRUE(check.result)) {
-    utils::install.packages("swirl", repos = repos)
-  }
+  utils::install.packages(c('stringr', 'stringi', 'testthat', 'httr', 'yaml', 'RCurl', 'digest', 'rappdirs', 'getPass'))
+  utils::install.packages("swirl", repos = repos)
   library(swirl)
   library(curl)
   library(methods)
